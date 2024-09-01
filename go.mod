4d500bccc00b309048d84d26d5f86a9510fccd0c
Forud Ghafouri <fzerorubigd@gmail.com>
2024-09-01 19:45:52 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/fzerorubigd/bg-tools
+
+go 1.23.0
+
+require (
+	github.com/chromedp/chromedp v0.10.0
+	github.com/fzerorubigd/gobgg v0.10.2
+	go.uber.org/ratelimit v0.3.1
+)
+
+require (
+	github.com/benbjohnson/clock v1.3.5 // indirect
+	github.com/chromedp/cdproto v0.0.0-20240810084448-b931b754e476 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"text/template"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"github.com/fzerorubigd/gobgg"
+	"go.uber.org/ratelimit"
+)
+
+//go:embed template/last_plays.html
+var tplText string
+
+type Single struct {
+	ID    int64
+	Row   int
+	Col   int
+	Alt   string
+	URL   string
+	Count int
+}
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGKILL,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+		syscall.SIGABRT)
+	defer cancel()
+	var (
+		username string
+		outfile  string
+	)
+
+	flag.StringVar(&username, "username", "fzerorubigd", "The BGG username")
+	flag.StringVar(&outfile, "out", "out.png", "The file to save the result")
+	flag.Parse()
+
+	rl := ratelimit.New(10, ratelimit.Per(60*time.Second)) // creates a 10 per minutes rate limiter.
+	bgg := gobgg.NewBGGClient(gobgg.SetLimiter(rl))
+	plays, err := bgg.Plays(ctx, gobgg.SetUserName(username), gobgg.SetPageNumber(1))
+	if err != nil {
+		panic(err)
+	}
+	cc := make([]Single, 16)
+	row, col := 1, 0
+bigLoop:
+	for _, play := range plays.Items {
+		if play.Quantity < 1 {
+			continue
+		}
+		for i := range cc {
+			if cc[i].ID == play.Item.ID {
+				cc[i].Count = cc[i].Count + 1
+				continue bigLoop
+			}
+			if cc[i].ID == 0 {
+				col++
+				if col == 5 {
+					col = 1
+					row++
+				}
+				cc[i].Row = row
+				cc[i].Col = col
+				cc[i].ID = play.Item.ID
+				cc[i].Alt = play.Item.Name
+				continue bigLoop
+			}
+		}
+		break
+	}
+
+	idx := make([]int64, len(cc))
+	for i := range cc {
+		if cc[i].ID > 0 {
+			idx[i] = cc[i].ID
+		}
+	}
+
+	items, err := bgg.GetThings(ctx, gobgg.GetThingIDs(idx...))
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range cc {
+		for _, th := range items {
+			if cc[i].ID == th.ID {
+				cc[i].URL = th.Thumbnail
+			}
+		}
+	}
+
+	tpl := template.Must(template.New("last_plays").Parse(tplText))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+
+		tpl.Execute(w, cc)
+	})
+
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	addr := listener.Addr().(*net.TCPAddr).Port
+	fmt.Print(addr)
+	go func() {
+		http.Serve(listener, nil)
+	}()
+
+	var buf []byte
+	chromCtx, cancel := chromedp.NewContext(ctx)
+	defer cancel()
+	err = chromedp.Run(chromCtx,
+		chromedp.EmulateViewport(1024, 768),
+		chromedp.Navigate(fmt.Sprintf("http://127.0.0.1:%d", addr)),
+		chromedp.Screenshot("div.gallery", &buf, chromedp.NodeReady),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := os.WriteFile(outfile, buf, 0750); err != nil {
+		panic(err)
+	}
+}
